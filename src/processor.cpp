02db49db8edf6f316241df65244ab87b4686a4b5
Andrewzhang217 <zhangguangzesheng@gmail.com>
2022-07-05 14:10:22 +0800

    Remove std::future and use of thread_pool

--- src/processor.cpp
+++ src/processor.cpp
@@ -2,45 +2,34 @@
 
 #include <cstdint>
 #include <utility>
+#include <iostream>
 
 std::atomic<std::uint32_t> biosoup::NucleicAcid::num_objects{0};
 
 namespace ram_analyser {
-Processor::Processor(std::shared_ptr<thread_pool::ThreadPool> &pool,
-                     std::uint8_t kmer_len,
-                     std::uint8_t window_len,
-                     std::vector<std::unique_ptr<biosoup::NucleicAcid>> &targets) :
-        pool_(pool),
-        minimizer_engine_(pool, kmer_len, window_len),
-        targets_(targets) {
-    minimizer_engine_.Minimize(targets_.begin(), targets_.end(), false); // set dafault minhash to false
-    minimizer_engine_.Filter(0.001); // set default frequency to 0.001
-}
-std::vector<std::vector<biosoup::Overlap>> Processor::FindAvaOverlaps() {
-    return FindOverlaps(0, static_cast<int> (targets_.size()));
-}
-std::vector<std::vector<biosoup::Overlap>> Processor::FindOverlaps(int start, int end) {
-    std::uint32_t num_targets = biosoup::NucleicAcid::num_objects.load();
-    biosoup::NucleicAcid::num_objects = 0;
-    std::vector<std::vector<biosoup::Overlap>> results;
-    while (true) {
-        std::vector<std::future<std::vector<biosoup::Overlap>>> futures;
+    Processor::Processor(std::shared_ptr<thread_pool::ThreadPool> &pool,
+                         std::uint8_t kmer_len,
+                         std::uint8_t window_len,
+                         std::vector<std::unique_ptr<biosoup::NucleicAcid>> &targets) :
+            pool_(pool),
+            minimizer_engine_(pool, kmer_len, window_len),
+            targets_(targets) {
+        minimizer_engine_.Minimize(targets_.begin(), targets_.end(), false); // set dafault minhash to false
+        minimizer_engine_.Filter(0.001); // set default frequency to 0.001
+    }
+
+    std::vector<std::vector<biosoup::Overlap>> Processor::FindAvaOverlaps() {
+        return FindOverlaps(0, static_cast<int> (targets_.size()));
+    }
+
+    std::vector<std::vector<biosoup::Overlap>> Processor::FindOverlaps(int start, int end) {
+        std::uint32_t num_targets = biosoup::NucleicAcid::num_objects;
+        biosoup::NucleicAcid::num_objects = 0;
+        std::vector<std::vector<biosoup::Overlap>> results;
         for (auto it = start; it < end; ++it) {
             if (it >= num_targets) continue;
-            futures.emplace_back(pool_->Submit(
-                    [&](const std::unique_ptr<biosoup::NucleicAcid> &sequence)
-                            -> std::vector<biosoup::Overlap> {
-                        return minimizer_engine_.Map(sequence, true, false, false); // is_ava = true
-                    },
-                    std::ref(targets_[it])));
+            results.emplace_back(minimizer_engine_.Map(targets_[it], true, false, true));
         }
-        if (biosoup::NucleicAcid::num_objects >= num_targets) {
-            for (auto &it : futures) {
-                results.emplace_back(it.get());
-            }
-            break;
-        }
+        return results;
     }
-    return results;
-}
 } // namespace ram_analyser
--- test/test.cpp
+++ test/test.cpp
@@ -9,7 +9,7 @@
 
 namespace ram_analyser_testing {
 static const std::string
-        &input_file_path = "../test/nanosim_drosophila_ref_iso-profile_100k_extend_aligned_reads.fasta";
+        &input_file_path = "../../nanosim_drosophila_ref_iso-profile_100k_extend_aligned_reads.fasta";
 static const int k_mer_length = 5;
 static const int window_length = 5;
 //ram_analyser::Analyser analyser{input_file_path, k_mer_length, window_length};
@@ -18,11 +18,20 @@ TEST_CASE("Unit test: Input") {
     auto targets = input.Sequences();
     REQUIRE(!targets.empty());
 }
-TEST_CASE("Unit test: Processor") {
+
+TEST_CASE("Unit test: Minimizer_engine indexing") {
     ram_analyser::Input input{input_file_path};
     auto targets = input.Sequences();
     auto thread_pool = std::make_shared<thread_pool::ThreadPool>(10);
+    ram::MinimizerEngine minimizerEngine{thread_pool};
+    minimizerEngine.Minimize(targets.begin(), targets.end());
+}
+TEST_CASE("Unit test: Processor") {
+    ram_analyser::Input input{input_file_path};
+    auto targets = input.Sequences();
+    auto thread_pool = std::make_shared<thread_pool::ThreadPool>(20);
     ram_analyser::Processor processor{thread_pool, 5, 5, targets};
+    std::cout << "initialised" << std::endl;
     auto overlaps_first_1k = processor.FindOverlaps(0, 1000);
     std::cout << overlaps_first_1k.size() << std::endl;
     REQUIRE(!overlaps_first_1k.empty());
